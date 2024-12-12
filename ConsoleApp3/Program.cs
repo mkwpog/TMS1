f79be6b7d91c4f0ec4caed375f19fb12796c98de
mkwpog <mirek.cydzik97@gmail.com>
2024-12-12 23:31:59 +0100

    Загрузка файлов

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace ConsoleApp3
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Введите имя: ");
+            var firstName = Console.ReadLine();
+
+            Console.WriteLine("Введите фамилию: ");
+            var lastName = Console.ReadLine();
+
+            Console.WriteLine("Введите отчество: ");
+            var fatherName = Console.ReadLine();
+
+            Console.WriteLine("Введи дату рожднения (формат день.месяц.год):");
+            var Date = Console.ReadLine();
+            
+
+            Console.WriteLine($"Ваши данные: {firstName} {lastName} {fatherName} ,дата рождения: {Date}");
+
+            Console.ReadLine();
+            Console.ReadKey();
+
+        }
+    }
+}
